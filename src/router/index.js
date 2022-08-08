15ce79abf57550c2c8f96b1ec2acd5554416f711
JoasGomes <jojoca.vitor777@gmail.com>
2022-08-08 11:50:24 -0300

    iniciando projeto

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import Contato from '../views/Contato.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/contato',
+    name: 'contato',
+    component: Contato
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
