edd77ccc7a417dc83171b017d75b1c5f5995e8c6
JoasGomes <jojoca.vitor777@gmail.com>
2022-08-19 07:51:16 -0300

    Finalizado

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Contato from '../views/Contato.vue'
 import Cursos from '../views/Cursos.vue'
+import Curso from '../views/Curso.vue'
+import Aula from '../views/Aula.vue'
 
 const routes = [
   {
@@ -18,6 +20,20 @@ const routes = [
     path: '/cursos',
     name: 'cursos',
     component: Cursos
+  },
+  {
+    path: '/cursos/:curso',
+    name: 'curso',
+    component: Curso,
+    props: true,
+    children: [
+      {
+        path: ":aula",
+        name: "aula",
+        component: Aula,
+        props: true,
+      }
+    ]
   },
 ]
 
